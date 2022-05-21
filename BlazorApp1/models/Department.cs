147185308ed42c98eade549fe5ba9cdac606bb62
Mohammed-Elfeky <77442971+Mohammed-Elfeky@users.noreply.github.com>
2022-05-21 13:57:41 +0200

    d1

---
+++ BlazorApp1/models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorApp1.models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }=new List<Employee>();
+    }
+}
---
+++ BlazorApp1/models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp1.models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
